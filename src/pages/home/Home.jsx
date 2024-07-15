c42d05afdaa5f45077289500a37c4dcaf4881319
Sebastián Hernández <sebastian.her0548@gmail.com>
2024-07-15 18:11:28 -0400

    Hover a boton CV

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,8 @@ export default function Home() {
         <div className="home">
             <h1 className="home-h1">Sebastián, Full Stack Developer</h1>
             <h5 className="home-h5">Entusiasta por la programación y la tecnología</h5>
-            <button className="home-btn"><a href="https://drive.google.com/file/d/1xXJwHo_OPOCYjT-D3odNe2_8C0nVeIgC/view" target="_blank">Descarga mi CV </a></button>
+            <a className="home_a" href="https://drive.google.com/file/d/1xXJwHo_OPOCYjT-D3odNe2_8C0nVeIgC/view" target="_blank"><button className="home-btn">Descarga mi CV</button> </a>
+            
         </div>
     )
 }
